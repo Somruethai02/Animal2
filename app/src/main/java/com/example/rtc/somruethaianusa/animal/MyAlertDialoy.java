b2dd41e789e2f7747974c0a8db73d3043ad09393
Somruthai <Somruthai_muchan@hotmail.com>
2016-12-04 14:00:55 +0700

    question new

--- app/src/main/java/com/example/rtc/somruethaianusa/animal/MyAlertDialoy.java
+++ app/src/main/java/com/example/rtc/somruethaianusa/animal/MyAlertDialoy.java
@@ -1,8 +1,29 @@
 package com.example.rtc.somruethaianusa.animal;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+
 /**
  * Created by SmilecomputeR on 4/12/2559.
  */
 
-public class MyAlertDialoy {
+public class MyAlertDialog {
+    private AlertDialog.Builder objAlert;
+
+    public void NoChooseEveryThing(Context context) {
+        objAlert = new AlertDialog.Builder(context);
+        objAlert.setIcon(R.drawable.mytrue);
+        objAlert.setTitle("Please Choose Answer ?");
+        objAlert.setMessage("Please Choose Answer on Radiobutton");
+        objAlert.setCancelable(false);
+        objAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onCilck(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        objAlert.show();
+    }
 }
--- app/src/main/java/com/example/rtc/somruethaianusa/animal/question.java
+++ app/src/main/java/com/example/rtc/somruethaianusa/animal/question.java
@@ -10,18 +10,43 @@ public class question extends AppCompatActivity {
 
     private ImageView imvAnimal;
     private RadioGroup radAnswer;
+    private MyAlertDialog objMyAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         initialWidget();
-    }
+        radAnswer.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+            @Override
+         public void onCheckChanged(RadioGroup radioGroup, int i ){
+            switch (i) {
+                case R.id.rad1:
+                    strAnswer = "Parrot";
+                    break;
+                case R.id.rad2:
+                    strAnswer = "Burmese python";
+                    break;
+                case R.id.rad3:
+                    strAnswer = "Green turtle";
+                    break;
+                case R.id.rad4:
+                    strAnswer = "curve-toed gecko";
+                    break;
+                default:
+                    strAnswer = null;
+                    break;
+            }
+        }
+    });
     private void initialWidget(){
         imvAnimal = (ImageView) findViewById(R.id.imvAnimal);
         radAnswer =(RadioGroup) findViewById(R.id.radAnswer);
     }
     public void  clickAnswer(View view){
+        checkChooseAnswer(){
 
+        }
+    private void ch
     }
 }
